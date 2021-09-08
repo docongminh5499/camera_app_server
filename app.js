8f8076e1f6e26a1ad3a7fd6e0e687960da429eef
Cong Minh Do <meo123zzz@gmail.com>
2021-09-08 14:39:13 +0700

    fix payload too large

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json({ limit: '50mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
 
 // Login
 app.post("/login", async (req, res) => {
